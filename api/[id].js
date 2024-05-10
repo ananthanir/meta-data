289ca78e3c54158a9d0c5451fdc23ed891fb6bcd
Ananthan <ananthan.rajasekharan@gmail.com>
2024-05-10 12:11:06 +0530

    Update [id].js

--- api/[id].js
+++ api/[id].js
@@ -3,7 +3,7 @@ const MDModel = require("../MDMode");
 
 module.exports = async (req, res) => {
     const { id } = req.query;
-    const [nid] = req.query.id.split('.');
+    const [nid] = id.split('.');
     await mongoose.connect("mongodb+srv://ananthan:ananthan@cluster0.apq2bmf.mongodb.net/NFTMetaData?retryWrites=true&w=majority");
     MDModel.find({nft_id: nid}).then((data) => {
         res.json(JSON.parse(data[0].metadata));
